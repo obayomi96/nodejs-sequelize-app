7ff0b1c1ce015f118e04179ddca079d4f5db388f
obayomi96 <martinsoluwaseun47@gmail.com>
2019-03-15 13:23:29 +0100

    Heroku postgres

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
-module.exports = new Sequelize('codegig', 'postgres', 'pg1996', {
-  host: 'localhost',
-  dialect: 'postgres',
+module.exports = new Sequelize('d2vts73s2gvpru', 'jzrtvjwhmxinrd', 'db7f8f396c378ce53d07dacee3e73e20c89206dfcebd2335bebb73434a4af76c', {
+  host: 'ec2-23-23-241-119.compute-1.amazonaws.com',
+  dialect: 'jzrtvjwhmxinrd',
   operatorsAliases: false,
 
   pool: {
